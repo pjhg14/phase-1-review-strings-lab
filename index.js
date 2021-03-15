2c05dd948a864cbec0c1d8eab7ba8c3ff987e4d7
pj <pjhg14@gmail.com>
2021-03-15 11:41:49 -0400

    commit tree ffdba956605520e3618436dadc1341ceca79acfc

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "PJ";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!` ;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
